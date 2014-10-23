f45f06ddee626d1a864db8bdd81c4044c55ef45c
SunPeter <ssd3237649@gmail.com>
2014-10-24 00:29:49 +0800

    fix

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
 var routes = require('./routes/index');
 var http = require('http');
 var path = require('path');
 var app = express();
+app.listen(process.env.PORT || 5000);
 
 // all environments
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.bodyParser({ keepExtensions: true, uploadDir: './public/DFS' }));
 app.use(express.methodOverride());
@@ -24,11 +18,4 @@ app.use(require('less-middleware')('/less', {
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 routes.route(app);
-http.createServer(app).listen(process.env.PORT || 5000, function(){
-  console.log('Express server start successfully');
-});
